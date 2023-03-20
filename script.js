5581121035974a720cc230708a0608c548c22916
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-03-20 20:58:53 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let myVeryLongName = "JavaScript!";
+const MY_BIRTHDATE = "01-02-1983";
+console.log(myVeryLongName);
+console.log(MY_BIRTHDATE);
+// Exercise for the variable
+
+// Working with variables
+
+let name = "John";
+admin = name;
+console.log(admin);
+
+let x = "10";
+let y = 20;
+let z = x + y;
+console.log(z);
+
+let myNumber = 32;
+myNumber.toString(32);
+console.log(myNumber);
+
+const MY_INT = 5;
+const MY_FLOAT = 6.667;
+console.log(MY_INT * MY_FLOAT);
